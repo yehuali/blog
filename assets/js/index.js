5384500574470997f2dd115400c70cfc1690b008
zhangjie <731255090@qq.com>
2019-04-03 10:07:41 +0800

    博客模板

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+#follow {
+    color: #fff;
+    width: 150px;
+    height: 40px;
+    display: block;
+    font-size: 14px;
+    line-height: 40px;
+    text-align: center;
+    margin: 20px auto 10px;
+    background: #38b7ea;
+    border-radius: 20px;
+    transition: 0.2s ease;}
